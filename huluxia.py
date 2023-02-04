bb38cd4df82c322e944726d5df01b4ba8338e0fe
ljili2003 <79502189+ljili2003@users.noreply.github.com>
2023-02-04 14:48:23 +0800

    Update huluxia.py

--- huluxia.py
+++ huluxia.py
@@ -64,7 +64,7 @@ def sign_in(key):
             exp = requests.post(url=urk,data={'_key': key,'cat_id': cat['categoryID']},headers=headers).json()
             # 签到板块
             print('签到成功获得经验:',exp['experienceVal'])
-            text=text+"签到成功获得经验:",exp['experienceVal']+'\n'
+            text=text+"签到成功获得经验:",f"exp['experienceVal']"+'\n'
 
 
 
